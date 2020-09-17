c724fa00e362d5ea3be41adb9d310a6fdd448f6b
Stewart Bell <stewbell@gmail.com>
2020-09-17 20:32:47 +1000

    working prediction

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 exports.helloWorld = (req, res) => {
-  res.send('Hello, World');
+ 
+
+res.set('Access-Control-Allow-Origin', '*');
+
+if (req.method === 'OPTIONS') {
+  // Send response to OPTIONS requests
+  res.set('Access-Control-Allow-Methods', 'PUT');
+  res.set('Access-Control-Allow-Headers', '*');
+  res.set('Access-Control-Max-Age', '3600');
+  res.status(204).send('');
+} else {
+  inJSON = req.body
+  const axios = require('axios');
+
+  axios.post('https://smash-epny2zfy7q-ts.a.run.app/predict', inJSON)
+  .then(function (response) {
+    console.log(response.data);
+    res.json(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+
+  
+ 
+  //res.json(inJSON);
+}
 };
